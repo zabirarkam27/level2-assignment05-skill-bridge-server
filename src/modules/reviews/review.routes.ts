6ecb3e4d988e20d893ebd3fe53cc079190725c35
Zabir Arkam Akhond <zabirarkam27@gmail.com>
2026-04-30 19:43:50 +0600

    adding tutor-me route to review.route page

--- src/modules/reviews/review.routes.ts
+++ src/modules/reviews/review.routes.ts
@@ -5,6 +5,7 @@ import { ReviewController } from "./review.controller";
 const router = Router();
 
 router.post("/", auth(UserRole.STUDENT), ReviewController.createReview);
+router.get("/tutor-me", auth(UserRole.TUTOR), ReviewController.getTutorOwnReviews);
 router.get("/tutor/:tutorId", ReviewController.getTutorReviews);
 router.get("/student", auth(UserRole.STUDENT), ReviewController.getStudentReviews);
 
